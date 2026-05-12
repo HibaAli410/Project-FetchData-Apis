b10ce81db111012858277e6cfa1102acd838a4c0
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-13 00:54:07 +0500

    Divide post into small pieces and assign variable and store them

--- function.js
+++ function.js
@@ -3,6 +3,15 @@ async function getData(){
         let response = await fetch("https://jsonplaceholder.typicode.com/posts");
         let data = await response.json();
         console.log(data);
+        data.slice(0,20).forEach((post)=>{
+            let postTitle = post.title;
+            let postBody = post.body;
+            let postUserId = post.userID;
+            // console.log("Title of Post is " + post.title);
+            // console.log("Body of Post is " + post.body);
+            // console.log("UserId of Post is " + post.userId);
+            displayPost()
+        })
         
     }
     catch(error){
@@ -10,3 +19,6 @@ async function getData(){
     }
     }
     getData();
+    function displayPost(userID,postId){
+
+    }
