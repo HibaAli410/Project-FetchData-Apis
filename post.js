f97f6eb445e1aac07574e1f98bacf02b6ebfde58
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-13 19:40:36 +0500

    Extra whiteSpace removing

--- post.js
+++ post.js
@@ -1,55 +1,52 @@
-const params = new URLSearchParams(location.search);
-console.log(params);
-const userId = params.get('userId');
+const params = new URLSearchParams(location.search)
+console.log(params)
+const userId = params.get('userId')
 //console.log(paramuserId);
-const id = params.get('id');
+const id = params.get('id')
 //console.log(parampostId);
 
-async function fetchPost(){
-    try{
+async function fetchPost() {
+    try {
         // Get Post
-        const UrlPost  = 'https://jsonplaceholder.typicode.com/posts?userId=' + userId + '&id=' + id;;
+        const UrlPost =
+            'https://jsonplaceholder.typicode.com/posts?userId=' +
+            userId +
+            '&id=' +
+            id
         //console.log(URLpost);
-        const getPost = await fetch(UrlPost);
-        const response = await getPost.json();
+        const getPost = await fetch(UrlPost)
+        const response = await getPost.json()
 
         //console.log(URLauthor);
         response.forEach(data => {
-        //console.log(data.title);
-        const title = data.title;
-        const body = data.body;
-        displayPost(userId,id,title,body)
-        });
-        
-
-    }
-    catch(error){
-
+            //console.log(data.title);
+            const title = data.title
+            const body = data.body
+            displayPost(userId, id, title, body)
+        })
+    } catch (error) { 
+        console.log('Error:', error)
     }
 }
 // Call asynFunction
-fetchPost();
+fetchPost()
 
+// Get Author
 async function author() {
     try {
-        // Get Author
-        const UrlAuthor = 'https://jsonplaceholder.typicode.com/users?id=' + userId;
-
-        const getAuthor = await fetch(UrlAuthor);
-        const authorDetail = await getAuthor.json();
-        const authorName = authorDetail[0]["name"];
-        return authorName;
-        
+        const UrlAuthor = 'https://jsonplaceholder.typicode.com/users?id=' + userId
+        const getAuthor = await fetch(UrlAuthor)
+        const authorDetail = await getAuthor.json()
+        const authorName = authorDetail[0]['name']
+        return authorName
     } catch (error) {
-        console.log("Error:", error);
+        console.log('Error:', error)
     }
 }
 
-author();
-
 // Diplay Post Table
-async function displayPost(userId,id,title,body){
-const div = document.getElementById('container')
+async function displayPost(userId, id, title, body) {
+    const div = document.getElementById('container')
     const table = document.createElement('table')
     const thead = document.createElement('thead')
     table.appendChild(thead)
@@ -60,26 +57,25 @@ const div = document.getElementById('container')
     headerrow.appendChild(headingauthor)
     headerrow.appendChild(headingtitle)
     headerrow.appendChild(headingbody)
-    headingauthor.textContent = "Author"
+    headingauthor.textContent = 'Author'
     headingtitle.textContent = 'Title'
     headingbody.textContent = 'Detail'
     thead.appendChild(headerrow)
     table.appendChild(thead)
-    
+
     const row = document.createElement('tr')
     const tdauthor = document.createElement('td')
-  const tdtitle = document.createElement('td')
-  const tdbody = document.createElement('td')
-  tdauthor.textContent = await author();
-//     author().then(name=>{
-//     tdauthor.textContent = name;
-//    });
-  tdtitle.textContent = title;
-  tdbody.textContent = body;
-  row.appendChild(tdauthor);
-  row.appendChild(tdtitle);
-  row.appendChild(tdbody);
-  table.appendChild(row);
-  document.body.appendChild(table);
-
+    const tdtitle = document.createElement('td')
+    const tdbody = document.createElement('td')
+    tdauthor.textContent = await author()
+    //     author().then(name=>{
+    //     tdauthor.textContent = name;
+    //    });
+    tdtitle.textContent = title
+    tdbody.textContent = body
+    row.appendChild(tdauthor)
+    row.appendChild(tdtitle)
+    row.appendChild(tdbody)
+    table.appendChild(row)
+    document.body.appendChild(table)
 }
