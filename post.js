56b9e1130f0e3b2fc4ffd53f22e6edd91a5ec65b
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-13 09:23:28 +0500

    GetParams: get PostId and UserId

---
+++ post.js
@@ -0,0 +1,7 @@
+const params = new URLSearchParams(location.search);
+console.log(params);
+const paramuserId = params.get('userId');
+//console.log(paramuserId);
+const parampostId = params.get('postId');
+//console.log(parampostId);
+
