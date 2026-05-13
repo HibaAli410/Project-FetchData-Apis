e912815b2103728ce32e4a6f3377befaddf190fd
Hiba Ali <batulkhanzada111@gmail.com>
2026-05-13 10:10:40 +0500

    fetch Post from jsonplaceholder by params and create table to display post onit

--- post.js
+++ post.js
@@ -1,7 +1,50 @@
 const params = new URLSearchParams(location.search);
 console.log(params);
-const paramuserId = params.get('userId');
+const userId = params.get('userId');
 //console.log(paramuserId);
-const parampostId = params.get('postId');
+const id = params.get('id');
 //console.log(parampostId);
 
+async function fetchPost(){
+    try{
+        const URL = 'https://jsonplaceholder.typicode.com/posts?userId=' + userId + '&id=' + id;
+        //console.log(URL);
+        const getPost = await fetch(URL);
+        const response = await getPost.json();
+        console.log(response);
+        const title = response.title;
+        const body = response.body;
+        displayPost(userId,id,title,body)
+
+    }
+    catch(error){
+
+    }
+}
+fetchPost();
+function displayPost(userId,id,title,body){
+const div = document.getElementById('container')
+    const table = document.createElement('table')
+    const thead = document.createElement('thead')
+    table.appendChild(thead)
+    const headerrow = document.createElement('tr')
+    const headingtitle = document.createElement('th')
+    const headingbody = document.createElement('th')
+    headerrow.appendChild(headingtitle)
+    headerrow.appendChild(headingbody)
+    headingtitle.textContent = 'Post Title'
+    headingbody.textContent = 'Post Detail'
+    thead.appendChild(headerrow)
+    table.appendChild(thead)
+    
+    const row = document.createElement('tr')
+  const tdtitle = document.createElement('td')
+  const tdbody = document.createElement('td')
+  tdtitle.textContent = title;
+  tdbody.textContent = body;
+  row.appendChild(tdtitle);
+  row.appendChild(tdbody);
+  table.appendChild(row);
+  document.body.appendChild(table);
+
+}
